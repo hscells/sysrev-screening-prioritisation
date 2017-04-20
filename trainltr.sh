41927457ce385da47f5ed1ff750e055dc4944f8e
Harry Scells <harryscells@gmail.com>
2017-04-20 12:45:03 +1000

    added to training pipeline

--- trainltr.sh
+++ trainltr.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
 
+# Get set the ranklib path, and build the jar if it does not exist
+ranklib_path=./ranklib/build/libs/ranklib.jar
+if [ ! -f ${ranklib_path} ]; then
+    cd ./ranklib/
+    ./gradlew build
+    cd ../
+fi
+
+# Create training data by using elasticsearch to generate features for RankLib
+python3 ./ltrfeatures.py --mapping ./pmid-mapping.json --queries ./elastic-pico-queries.json > training.txt
+
+# Train a model
+java -jar ${ranklib_path} -train training.txt -ranker 6 -save model.txt
