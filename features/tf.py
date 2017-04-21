147ac0f6229d412c5bd1a5225a3280cd0608dc2d
Harry Scells <harryscells@gmail.com>
2017-04-21 11:32:48 +1000

    fix iteration bug

--- features/tf.py
+++ features/tf.py
@@ -11,7 +11,7 @@ class TermFrequencyFeature(AbstractTermFeature):
         document length and divide: TF(t)/DL
         """
         if self.term in self.term_statistics:
-            dl = sum(term['term_freq'] for term in self.term_statistics)
+            dl = sum([self.term_statistics[term]['term_freq'] for term in self.term_statistics])
             normalised_tf = self.term_statistics[self.term]['term_freq'] / dl
             return normalised_tf
         return 0.0
