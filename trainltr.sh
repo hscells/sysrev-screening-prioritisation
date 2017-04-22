02b56fb9a1839d07da4d39853506a3c46a1a54c2
Harry Scells <harryscells@gmail.com>
2017-04-22 13:07:15 +1000

    add evaluation to pipeline

--- trainltr.sh
+++ trainltr.sh
@@ -5,12 +5,24 @@ ranklib_path=./ranklib/build/libs/ranklib.jar # Path to RankLib
 model_name=model # Model name exported from RankLib _and_ used in elasticsearch
 elasticsearch_host=localhost
 elasticsearch_port=9200
+elasticsearch_index=med
+queries_json_file=./elastic-pico-queries.json
+output_dest=./results/
 
+# don't touch these variable
+elastic_address=${elasticsearch_host}:${elasticsearch_port} # calculated elasticsearch url
+timestamp=$(date | sed 's/ //g' | cut -c 1-14) # unique name for the file
+trec_baseline=${output_dest}trec_baseline_${timestamp}.txt
+trec_ltr=${output_dest}trec_ltr_${timestamp}.txt
+
+function log {
+    echo $(date) ' ' $1
+}
 # Check if elasticsearch is running before proceeding
 nc -zc ${elasticsearch_host} ${elasticsearch_port} &> /dev/null
 if [ $? -ne 0 ]
 then
-    echo 'elasticsearch is not running, or it is running on a different address than ${elasticsearch_host}:${elasticsearch_port}.'
+    log 'elasticsearch is not running, or it is not running on ${elastic_address}.'
     exit 1
 fi
 
@@ -21,12 +33,38 @@ if [ ! -f ${ranklib_path} ]; then
     cd ../
 fi
 
+log 'Extracting features...'
+
 # Create training data by using elasticsearch to generate features for RankLib
-python3 ./ltrfeatures.py --mapping ./pmid-mapping.json --queries ./elastic-pico-queries.json --output training.txt
+python3 ./ltrfeatures.py --mapping ./pmid-mapping.json --queries ${queries_json_file} --output training.txt
+
+log 'Training a model......'
 
 # Train a model
 # https://sourceforge.net/p/lemur/wiki/RankLib%20How%20to%20use/
 java -jar ${ranklib_path} -train training.txt -ranker 6 -save ${model_name}.txt -gmax 1 -metric2t NDCG@100 -tvs 0.2
 
+log 'Uploading model and searching...'
+
 # Now we can upload the model to elasticsearch
-python3 ./uploadscript.py --input ${model_name}.txt --elastic-url http://${elasticsearch_host}:${elasticsearch_port} -v
+python3 ./uploadscript.py --input ${model_name}.txt --elastic-url http://${elastic_address} -v
+
+python3 ./search.py \
+        -q ${queries_json_file} \
+        --baseline-output ${trec_baseline} \
+        --ltr-output ${trec_ltr} \
+        --elastic-url http://${elastic_address} \
+        --elastic-index ${elasticsearch_index}
+
+sort -o ${trec_baseline} ${trec_baseline}
+sort -o ${trec_ltr} ${trec_ltr}
+
+log 'Evaluating...'
+
+# the "baseline" is now done
+trec_eval -q qrels.txt ${trec_baseline} > ${output_dest}baseline-${timestamp}.txt
+
+# now, evaluate the fallback PICO
+trec_eval -q qrels.txt ${trec_ltr} > ${output_dest}ltr-${timestamp}.txt
+
+log 'Done!'
