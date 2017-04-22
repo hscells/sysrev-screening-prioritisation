423e28d1307aad704a6886408198c797297e5bd6
Harry Scells <harryscells@gmail.com>
2017-04-22 14:09:23 +1000

    add more idf features

--- features/idf.py
+++ features/idf.py
@@ -1,17 +1,41 @@
+import numpy as np
+from typing import List
+
 from features.feature import AbstractFeature
 
 
 class IDFSum(AbstractFeature):
-    """
-    
-    """
-
-    def calc(self) -> float:
-        total = 0.0
+    def idf_weights(self) -> List[float]:
         for term in self.query_vocabulary:
             if term in self.term_statistics:
-                total += self.field_statistics['doc_count'] / self.term_statistics[term]['ttf']
-        return total
+                yield self.field_statistics['doc_count'] / self.term_statistics[term]['ttf']
+
+    def calc(self) -> float:
+        return sum(self.idf_weights())
 
     def feature_manager_id(self) -> int:
         return 1
+
+
+class IDFStd(IDFSum):
+    def calc(self) -> float:
+        return np.std(self.idf_weights())
+
+    def feature_manager_id(self):
+        return 2
+
+
+class IDFMax(IDFSum):
+    def calc(self) -> float:
+        return max(self.idf_weights())
+
+    def feature_manager_id(self):
+        return 4
+
+
+class IDFMean(IDFSum):
+    def calc(self) -> float:
+        return np.mean(self.idf_weights())
+
+    def feature_manager_id(self):
+        return 5
