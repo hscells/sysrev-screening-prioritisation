bc6abb624dcef4a6a93268f69df7f5037334dc0f
Harry Scells <harryscells@gmail.com>
2017-05-09 15:57:04 +1000

    Made elastic-plugin path configurable

--- installPlugin.sh
+++ installPlugin.sh
@@ -2,11 +2,12 @@
 
 # Comment this line out if your JAVA_HOME is somewhere else.
 export JAVA_HOME=$(/usr/libexec/java_home)
+plugin_path=~/elasticsearch/elasticsearch-5.3.0/bin/elasticsearch-plugin
 
 # Build the plugin
 cd ltr-query
 ./gradlew run#installLtrQueryPlugin
 
 # Copy it into the elasticsearch plugin folder. This requires `elasticsearch-plugin` to be on your PATH.
-elasticsearch-plugin -v install file://$(pwd)/build/distributions/ltr-query-0.1.1-es5.3.0.zip
+${plugin_path} -v install file://$(pwd)/build/distributions/ltr-query-0.1.1-es5.3.0.zip
 cd ../
